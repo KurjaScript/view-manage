cc37127c46e86f5c613bbc2787e1b3e9a2c5f8e7
Kurja <kurja0403@gmail.com>
2022-03-11 01:58:32 +0800

    修改menu样式

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
         <h3>通用后台管理系统</h3>
         <el-menu-item v-for="item in noChildren" :index="item.path" :key="item.path">
             <i :class="'el-icon-' + item.icon"></i>
@@ -16,12 +16,6 @@
         </el-submenu>
     </el-menu>
 </template>
-<style>
-  .el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 200px;
-    min-height: 400px;
-  }
-</style>
 
 <script>
   export default {
@@ -88,3 +82,19 @@
     }
   }
 </script>
+
+<style lang="less" scoped> 
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .el-menu {
+      height: 100%;
+      border: none;
+      h3 {
+          color: #fff;
+          text-align: center;
+          line-height: 48px;
+      }
+  }
+</style>
