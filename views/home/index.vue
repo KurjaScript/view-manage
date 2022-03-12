9ddc113f61c643d692ac58f724b6b11f34a17b87
Kurja <kurja0403@gmail.com>
2022-03-12 19:07:17 +0800

    home组件布局

--- views/home/index.vue
+++ views/home/index.vue
@@ -1,11 +1,28 @@
 <template>
-    <div>我是home页面</div>
+    <el-row class="home" :getter="20">
+        <el-row :span="8" style="margin-top: 20px">
+            <el-card shadow="hover">
+                <div class="user">
+                    <img :src="userImg" />
+                    <div class="userInfo">
+                        <p class="name">Admin</p>
+                        <p class="access">超级管理员</p>
+                    </div>
+                </div>
+                <div class="login-info">
+                    <p>上次登录时间:<span>2021-12-24</span></p>
+                    <p>上次登录地点:<span>南京</span></p>
+                </div>
+            </el-card>
+        </el-row>
+    </el-row>
 </template>
 <script>
 export default {
     name: 'HoMe',
     data () {
         return {
+            userImg: require('../../src/assets/images/user.png')
 
         }
     }
