c4f3bdf336fea6a2239554625a8b23137e05bc97
Kurja <Kurja0403@gmail.com>
2022-03-29 01:36:35 +0800

    面包屑实现

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -2,7 +2,10 @@
     <header>
         <div class="l-content">
             <el-button @click="handleMenu" plain icon="el-icon-menu" size="mini"></el-button>
-            <h3 style="color: #fff">首页</h3>
+            <!-- <h3 style="color: #fff">首页</h3> -->
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item v-for="item in tags" :key="item.path" :to="{ path: item.path }">{{item.label}}</el-breadcrumb-item>
+            </el-breadcrumb>
         </div>
         <div class="r-content">
             <el-dropdown trigger="click" size="mini">
@@ -18,12 +21,18 @@
     </header>
 </template>
 <script>
+import { mapState } from 'vuex'
 export default {
     name: 'CommonHeader',
     data() {
         return {
             userImg: require('../assets/images/user.png')
         }
+    },
+    computed: {
+        ...mapState({
+            tags: state => state.tab.tabsList
+        })
     },
     methods: {
         handleMenu(){
