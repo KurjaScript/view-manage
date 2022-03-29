eda0683440aea131f4fd5a0c759b74d9efa94c01
Kurja <kurja0403@gmail.com>
2022-03-29 19:52:20 +0800

    新增、搜索样式调整

--- views/User/index.vue
+++ views/User/index.vue
@@ -112,3 +112,12 @@ export default {
     }
 }
 </script>
+<style lang="less" scoped>
+    
+    .manage-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    
+</style>
