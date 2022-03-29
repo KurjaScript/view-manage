9a42c7d7dadd71dabff7d062e09decc47e1e5ec0
Kurja <kurja0403@gmail.com>
2022-03-29 17:05:46 +0800

    form 组件编写

---
+++ src/components/CommonForm.vue
@@ -0,0 +1,47 @@
+<template>
+    <el-form ref="form" label-width="100px" :model="form" :inline="inline">
+        <el-form-item v-for="item in formLabel" :key="item.label" :label="item.label">
+            <el-input
+                v-if="item.type === 'input'"
+                :placeholder="'请输入' + item.label"
+                v-model="form[item.model]"
+            >
+            </el-input>
+            <el-switch v-if="item.type === 'switch'" v-model="form[item.model]"></el-switch>
+            <el-date-picker
+                v-if="item.type === 'date'"
+                type="date"
+                value-format="yyyy-MM-dd"
+                placeholder="选择日期"
+                v-model="form[item.model]"
+            ></el-date-picker>
+            <el-select
+                v-if="item.type === 'select'"
+                placeholder="请选择"
+                v-model="form[item.model]"
+            >
+                <el-option 
+                    v-for="item in item.opts"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                ></el-option>     
+            </el-select>
+        </el-form-item>
+        <el-form-item><slot></slot></el-form-item>
+    </el-form>
+
+</template>
+<script>
+export default {
+    name: 'CommonForm',
+    props: {
+        formLabel: Array, // 接收 form 的相关配置
+        form: Object, // 接收 form 的数据
+        inline: Boolean, // 表单的布局
+    },
+    data() {
+
+    }
+}
+</script>
