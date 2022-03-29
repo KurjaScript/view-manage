ce21a0e8be7b67cbd0e55d0044d00cc64c6c2c36
Kurja <kurja0403@gmail.com>
2022-03-29 20:33:14 +0800

    编辑接口调用

--- api/mock.js
+++ api/mock.js
@@ -1,5 +1,8 @@
 import Mock from 'mockjs'
 import homeApi from './mockServeData/home.js'
+import userApi from './mockServeData/user'
 
 // 调用
-Mock.mock('/home/getData', homeApi.getStatisticalData,)
+Mock.mock('/home/getData', homeApi.getStatisticalData)
+Mock.mock(/user\/add/, 'post', userApi.createUser)
+Mock.mock(/user\/edit/, 'post', userApi.updateUser)
