01a5e99ce5cec7d7498997094d517294e60196da
Kurja <kurja0403@gmail.com>
2022-03-30 15:41:51 +0800

    tabel组件的使用

--- api/mock.js
+++ api/mock.js
@@ -1,8 +1,11 @@
 import Mock from 'mockjs'
 import homeApi from './mockServeData/home.js'
-import userApi from './mockServeData/user'
+import userApi from './mockServeData/user.js'
 
 // 调用
 Mock.mock('/home/getData', homeApi.getStatisticalData)
 Mock.mock(/user\/add/, 'post', userApi.createUser)
 Mock.mock(/user\/edit/, 'post', userApi.updateUser)
+
+Mock.mock(/user\/getUser/, 'get', userApi.getUserList)
+Mock.mock(/user\/del/, 'post', userApi.deleteUser)
--- views/User/index.vue
+++ views/User/index.vue
@@ -26,14 +26,25 @@
                 <el-button type="primary" @click="getList">搜索</el-button>
             </common-form>
         </div>
+        <common-table
+            :tableData="tableData"
+            :tableLabel="tableLabel"
+            :config="config"
+            @changePage = "getList()"
+            @edit="editUser"
+            @del="delUser"
+        ></common-table>
     </div>
 </template>
 <script>
-import CommonForm from '../../src/components/CommonForm.vue'
+import CommonForm from '../../src/components/CommonForm.vue';
+import CommonTable from '../../src/components/CommonTable.vue';
+import {getUser} from  '../../api/data'
 export default {
     name: 'UsEr',
     components: {
         CommonForm,
+        CommonTable,
     },
     data () {
         return {
@@ -88,8 +99,36 @@ export default {
             ],
             searchForm: {
                 keyword: '',
+            },
+            tableData: [],
+            tableLabel: [
+                {
+                    prop: "name",
+                    label: "姓名",
+                },{
+                    prop: "age",
+                    label: "年龄",
+                },{
+                    prop: "sex",
+                    label: "性别",
+                },{
+                    prop: "birth",
+                    label: "出生日期",
+                    width: 200,
+                },{
+                    prop: "addr",
+                    label: "地址",
+                    width: 320,
+                },
+            ],
+            config: {
+                page: 1,
+                total: 30,
             }
         }
+    },
+    created(){
+        this.getList()
     },
     methods: {
         confirm() {
@@ -97,11 +136,13 @@ export default {
                 this.$http.post('/user/edit', this.operateForm).then(res => {
                     console.log(res)
                     this.isShow = false
+                    this.getList()
                 })
             } else {
                 this.$http.post('/user/add', this.operateForm).then(res => {
                     console.log(res)
                     this.isShow = false
+                    this.getList()
                 })
             }
         },
@@ -116,10 +157,47 @@ export default {
                 sex: '',
             };
         },
-        getList(){
-
+        getList(name = ''){
+            this.config.loading = true
+            name ? (this.config.page = 1) : ''
+            getUser({
+               page: this.config.page,
+               name
+            }).then(res => {
+                this.tableData = res.data.list.map( item => {
+                    item.sexLabel = item.sex === 0 ? "女" : "男"
+                    return item
+                })
+                this.config.total = res.count
+                this.config.loading = false
+            })
         },
-    }
+        changePage() {},
+        editUser(row) {
+            this.operateType = 'edit'
+            this.isShow = true
+            this.operateForm = row
+        },
+        delUser() {
+            this.$confirm("此操作将永久删除该文件，是否继续？", "提示", {
+                confirmButtonText: "确认",
+                cancelButtonText: "取消",
+                type: "warning"
+            }).then(() => {
+                const id = row.id
+                this.$http.get("/user/del", {
+                    params: { id }
+                }).then( () => {
+                    this.$message({
+                        type: 'success',
+                        message: '删除成功'
+                    })
+                    this.getList()
+                })
+            })
+        },
+      
+    },
 }
 </script>
 <style lang="less" scoped>
